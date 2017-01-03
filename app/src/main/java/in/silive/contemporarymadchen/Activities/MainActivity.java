336d57d5424586f66b9d9eef1408033b10f5179e
akriti <akritikts@gmail.com>
2017-01-03 21:11:58 +0530

    modules added

--- app/src/main/java/in/silive/contemporarymadchen/Activities/MainActivity.java
+++ app/src/main/java/in/silive/contemporarymadchen/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package in.silive.contemporarymadchen;
+package in.silive.contemporarymadchen.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import in.silive.contemporarymadchen.R;
 
 public class MainActivity extends AppCompatActivity {
 
---
+++ app/src/main/java/in/silive/contemporarymadchen/Fragments/Security.java
@@ -0,0 +1,30 @@
+package in.silive.contemporarymadchen.Fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import in.silive.contemporarymadchen.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Security extends Fragment {
+
+
+    public Security() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_security, container, false);
+    }
+
+}
